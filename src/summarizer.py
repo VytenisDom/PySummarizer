ecbdae3ac5cd188d103a2bb0ddafcea327181a38
VDOMASEVICIUS <vytenis.domasevicius@innoforcegroup.com>
2022-04-23 15:16:59 +0300

    Fixed scoring algorithm, now punishes long sentences looser, fixed sorting

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,6 +1,7 @@
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
 from string import punctuation
+import operator
 nlp = spacy.load("en_core_web_sm")
 # extractive summary by word count
 #text = """This is an example text. We will use seven sentences and we will return 3. This blog is written by Yujian Tang. Yujian is the best software content creator. This is a software content blog focused on Python, your software career, and Machine Learning. Yujian's favorite ML subcategory is Natural Language Processing. This is the end of our example."""
@@ -16,46 +17,54 @@ The sinking of the missile cruiser is one of the defining events of the war in U
 And now, the wreckage of the pride of Russia's fleet has been declared an item of Ukrainian underwater cultural heritage, under the category of rare scientific or technical equipment.
 
 As the BBC's Joe Inwood reports, Ukraine's Ministry of Defence says the wreck can be admired "without much diving"."""
-# tokenize
-doc = nlp(text)
-# create dictionary
-word_scores = {}
 
-# Add all words, that aren't stop words or punctuation to the word_scores object 
-for word in doc:
-    word_scores[word.text.lower()] = 0
+def summarize(text, n):
+    # tokenize
+    doc = nlp(text)
+    
+    # create dictionary
+    word_scores = {}
 
-
-# loop through every sentence and give it a weight
-for word in doc:
-    word = word.text.lower()
-    if word in word_scores and word not in STOP_WORDS and word not in punctuation and "\n" not in word:
-        word_scores[word] += 1
-
-# create a list of tuple (sentence text, score, index)
-sents = []
-# score sentences
-sent_score = 0
-#print(word_scores)
-for index, sent in enumerate(doc.sents):
-    for word in sent:
-        sent_score += word_scores[word.text.lower()]
-        #print(word_scores[word.text.lower()])
-    sents.append((sent.text.replace("\n", " "), sent_score / (len(sent) / 2), index))
-    #print(sent.text.replace("\n", " "), sent_score ,len(sent), sent_score / (len(sent) / 2))
-    sent_score = 0;
+    # Add all words, that aren't stop words or punctuation to the word_scores object 
+    for word in doc:
+        word_scores[word.text.lower()] = 0
 
 
-# sort sentence by word occurrences
-#print("----- \n", sents)
-sents = sorted(sents, key=lambda x: -x[1])
-#print("----- \n", sents)
-# return best rated n amount of sentences
-sents = sorted(sents[:n], key=lambda x: x[2])
+    # loop through every sentence and give it a weight
+    for word in doc:
+        word = word.text.lower()
+        if word in word_scores and word not in STOP_WORDS and word not in punctuation and "\n" not in word:
+            word_scores[word] += 1
 
-# compile them into text
-summary_text = ""
-for sent in sents:
-    summary_text += sent[0] + " "
- 
-print(summary_text)
+    # create a list of tuple (sentence text, score, index)
+    sents = []
+    # score sentences
+    sent_score = 0
+    #print(word_scores)
+    for index, sent in enumerate(doc.sents):
+        for word in sent:
+            sent_score += word_scores[word.text.lower()]
+            #print(word_scores[word.text.lower()])
+        sents.append((sent.text.replace("\n", " "), sent_score / (len(sent) / 2), index))
+        #print(sent.text.replace("\n", " "), sent_score ,len(sent), sent_score / (len(sent) / 2))
+        sent_score = 0;
+
+
+    # sort the sentences by word occurance(tuple index 1)
+    #print("----- \n", sents)
+    sents.sort(key=operator.itemgetter(1))
+    #print("----- \n", sents)
+    # return best rated n amount of sentences
+    bestSents = sents[-n:]
+    # keep the sentence order as in original text
+    bestSents.sort(key=operator.itemgetter(2))
+
+    # compile them into text
+    summary_text = ""
+    for sent in bestSents:
+        summary_text += sent[0] + " "
+        #print(sent[1])
+    
+    print(summary_text)
+
+summarize(text, n)
